bf5d2aff3ca4cbca4a0c309686e46de4b4414382
David Williams <davew1982@gmail.com>
2014-01-06 12:49:53 -0500

    Initial repository with a few changes to application controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MailchimpTugg::Application.config.secret_key_base = '9662cf77dd60db9e1b662728748ac29938503e415c47d76d225d9f2b42a273f95055c37ddc08cf80b07852239fadc24e7b464b0231611fff091926866068af5a'
